a811509ac66342206c99e3198a4751c4bd56dc6f
Andres <andrsbtrg@gmail.com>
2024-06-07 22:15:18 +0200

    Add get some data from speckle

--- mapple/__init__.py
+++ mapple/__init__.py
@@ -1,4 +1,8 @@
 from typing import Self
+from specklepy.api.client import SpeckleClient
+from specklepy.api.credentials import get_default_account
+from specklepy.api import operations
+from specklepy.transports.server.server import ServerTransport
 
 
 class Chainable:
@@ -23,10 +27,29 @@ class Chainable:
 def get(*args) -> Chainable:
     """Mostly do speckle queries and then
     save them inside the Chainable object"""
+
+    print(args)
+
     # get something
+    client = SpeckleClient(host='https://latest.speckle.systems')
+    # authenticate the client with a token
+    account = get_default_account()
+    client.authenticate_with_account(account)
+
+    stream_id = "24fa0ed1c3"
+    # print(client)
+    # stream = client.stream.get(id=stream_id)
+
+    transport = ServerTransport(client=client, stream_id=stream_id)
+
+    last_obj_id = client.commit.list(stream_id)[0].referencedObject
+    last_obj = operations.receive(
+        obj_id=last_obj_id, remote_transport=transport)
+
+    # debug me!
+    print(last_obj)
 
     something = "some data to assert"
-    print(args)
     return Chainable(something)
 
 
