f0ed9371939c2bde487659cff7937250ae60f98e
Gizem <gizemdemirhaan@gmail.com>
2024-06-08 18:04:45 +0200

    act arrange and assert on different lines

--- mapple_test.py
+++ mapple_test.py
@@ -4,8 +4,10 @@ import mapple as mp
 def spec_a():
     mp.it("Windows should be at least 1.70 m")
 
-    mp.get('category', 'Windows').where('speckle_type',
-                                        'Objects.Other.Instance:Objects.Other.Revit.RevitInstance').its('Height').should('be.greater', 1.70)
+    windows = mp.get('category', 'Windows')\
+        .where('speckle_type','Objects.Other.Instance:Objects.Other.Revit.RevitInstance')\
+        .its('Height')\
+        .should('be.greater', 1.70) # assert
 
 
 def spec_b():
