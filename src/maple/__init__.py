b091f39ef2890bd5e2ea8febe7704acdbf286f68
Andres <andrsbtrg@gmail.com>
2024-06-16 20:17:29 +0200

    Use getters for globals
    Will make it easier in the future to refactor and remove globals
    entirely

--- src/maple/__init__.py
+++ src/maple/__init__.py
@@ -29,12 +29,79 @@ class Result:
         self.assertions: list[Assertion] = []
 
 
-"""
-Important: this contains the results of the runs
-"""
-test_cases: list[Result] = []
+# GLOBALS
+# TODO: Refactor to remove globals
+test_cases: list[Result] = []  # NOTE: this contains the results of the runs
 current_object: Base = None
 stream_id: str = ""
+
+
+def init(obj: Base) -> None:
+    """
+    Sets the obj as current object
+    """
+    global current_object
+    current_object = obj
+    return
+
+
+def stream(id: str) -> None:
+    """
+    Set the stream_id
+    """
+    global stream_id
+    stream_id = id
+    return
+
+
+def get_token():
+    """
+    Get the token to authenticate with Speckle.
+    The token should be under the env variable 'SPECKLE_TOKEN'
+    """
+    import os
+
+    token = os.getenv("SPECKLE_TOKEN")
+    if token is None:
+        raise Exception("Expected `SPECKLE_TOKEN` env variable to be set")
+    return token
+
+
+def get_stream_id() -> str:
+    """
+    Gets the stream_id provided with mp.stream()
+    """
+    global stream_id
+    if stream_id == "":
+        raise Exception("Please provide a Stream id using mp.stream()")
+    return stream_id
+
+
+def get_current_obj() -> Base:
+    """
+    Get the current object specified with mp.init()
+    """
+    global current_object
+    return current_object
+
+
+def get_current_test_case() -> Result:
+    """
+    Get the current test case
+    """
+    global test_cases
+    current = test_cases[-1]
+    return current
+
+
+def get_test_cases() -> [Result]:
+    """
+    Gets the list of Test Cases
+    """
+    global test_cases
+    return test_cases
+
+# endof GLOBALS
 
 
 class Chainable:
@@ -49,8 +116,7 @@ class Chainable:
             self.assertion.it_passed(True)
         else:
             self.assertion.it_failed(True)
-        global test_cases
-        current = test_cases[-1]
+        current = get_current_test_case()
         current.assertions.append(self.assertion)
 
     def should(self, *args) -> Self:
@@ -93,8 +159,7 @@ class Chainable:
             else:
                 self.assertion.it_failed(objs[i].id)
 
-        global test_cases
-        current = test_cases[-1]
+        current = get_current_test_case()
         current.assertions.append(self.assertion)
         return self
 
@@ -132,8 +197,7 @@ class Chainable:
         filters more the Chainable
         """
         print("Filtering by:", *args)
-        global test_cases
-        current = test_cases[-1]
+        current = get_current_test_case()
         current.selected[args[0]] = args[1]
 
         selected = list(
@@ -145,15 +209,15 @@ class Chainable:
         return self
 
 
-def init(obj: Base):
-    global current_object
-    current_object = obj
+def it(test_name):
+    """
+    Declare a Spec and log it in the console
+    Also should create a new Queue of chainables
+    """
+    print("-------------------------------------------------------")
+    print("Running test:", test_name)
+    get_test_cases().append(Result(test_name))
     return
-
-
-def stream(id: str):
-    global stream_id
-    stream_id = id
 
 
 def get(*args) -> Chainable:
@@ -161,16 +225,16 @@ def get(*args) -> Chainable:
     save them inside the Chainable object"""
 
     print("Getting", *args)
-    global test_cases
-    test_cases[-1].selected[args[0]] = args[1]
+    current_test = get_current_test_case()
+    current_test.selected[args[0]] = args[1]
 
-    global current_object
-    if current_object is None:
-        last_commit = get_last_obj()
-    else:
-        last_commit = current_object
+    speckle_obj = get_current_obj()
+    if not speckle_obj:
+        speckle_obj = get_last_obj()
+    if speckle_obj is None:
+        raise Exception("Could not get a Base object to query.")
 
-    objs = list(flatten_base(last_commit))
+    objs = list(flatten_base(speckle_obj))
 
     selected = list(
         filter(lambda obj: property_equal(args[0], args[1], obj), objs))
@@ -179,41 +243,26 @@ def get(*args) -> Chainable:
     return Chainable(selected)
 
 
-def get_last_obj():
-    # get some objets
+def get_last_obj() -> Base:
+    """
+    Gets the last object for the specified stream_id
+    """
     client = SpeckleClient(host="https://latest.speckle.systems")
     # authenticate the client with a token
-    # account = get_default_account()
-    account = None
+    account = get_default_account()
     if account:
         client.authenticate_with_account(account)
     else:
         token = get_token()
         client.authenticate_with_token(token)
 
-    global stream_id
-    if stream_id == "":
-        raise Exception("Please provide a Stream id using mp.stream()")
-    # stream = client.stream.get(id=stream_id)
-
+    stream_id = get_stream_id()
     transport = ServerTransport(client=client, stream_id=stream_id)
 
     last_obj_id = client.commit.list(stream_id)[0].referencedObject
     last_obj = operations.receive(
         obj_id=last_obj_id, remote_transport=transport)
     return last_obj
-
-
-def it(test_name):
-    """
-    Declare a Spec and log it in the console
-    Also should create a new Queue of chainables
-    """
-    print("-------------------------------------------------------")
-    print("Running test:", test_name)
-    global test_cases
-    test_cases.append(Result(test_name))
-    return
 
 
 def run(*specs):
@@ -226,7 +275,7 @@ def print_results():
     print("-------------------------------------------------------")
     print("---RESULTS---")
     print("-------------------------------------------------------")
-    global test_cases
+    test_cases = get_test_cases()
     for case in test_cases:
         print(case.spec_name, end="")
         assertions = case.assertions
@@ -257,11 +306,3 @@ def evaluate(comparer, param_value, assertion_value):
             return round(float(param_value), 2) == round(float(assertion_value), 2)
     except Exception:
         return False
-
-
-def get_token():
-    import os
-    token = os.getenv("SPECKLE_TOKEN")
-    if token is None:
-        print("token is empty")
-    return token
