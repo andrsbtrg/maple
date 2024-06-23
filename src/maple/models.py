d0b42f377a5e554c982c1f8718cff850caab9651
Andres <andrsbtrg@gmail.com>
2024-06-23 17:13:20 +0200

    Removed unused property in Assertion

--- src/maple/models.py
+++ src/maple/models.py
@@ -1,19 +1,40 @@
 class Assertion:
+    """
+    Contains parameters and result of performing an assertion on
+    Speckle objects and its values
+
+    Attributes:
+        assertion_type:
+        value:
+        passed:
+        failed:
+
+    """
+
     def __init__(self) -> None:
-        self.param = ""
         self.assertion_type = ""
         self.value = None  # what will be compared to
         self.passed = []
-        self.failed = []
+        self.set_failed = []
 
-    def it_passed(self, obj_id):
+    def set_passed(self, obj_id):
         self.passed.append(obj_id)
 
-    def it_failed(self, obj_id):
-        self.failed.append(obj_id)
+    def set_failed(self, obj_id):
+        self.set_failed.append(obj_id)
 
 
 class Result:
+    """
+    Contains the Results of one Assertion
+
+    Attributes:
+        spec_name: name of the spec mp.it('name') who expects a result
+        selected: Dict with selector:value
+        assertions: List of assertions
+
+    """
+
     def __init__(self, spec_name: str) -> None:
         self.spec_name = spec_name
         self.selected = {}
