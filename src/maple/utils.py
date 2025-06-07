35a8d1e5f0c9274971b869a600b35db56dc0cd02
Andres <andrsbtrg@gmail.com>
2025-06-07 12:20:01 +0200

    chore: add flag to remove color output

--- src/maple/utils.py
+++ src/maple/utils.py
@@ -1,14 +1,24 @@
 from .models import Result
 
+import os
 
-RED = "\033[1;31m"
-BLUE = "\033[1;34m"
-CYAN = "\033[1;36m"
-GREEN = "\033[0;32m"
-RESET = "\033[0;0m"
-BOLD = "\033[;1m"
-REVERSE = "\033[;7m"
-ENDC = "\033[0m"
+
+def use_colors() -> bool:
+    no_color = os.getenv("NO_COLOR")
+    return no_color is None or no_color != "1"
+
+
+if use_colors():
+    RED = "\033[1;31m"
+    BLUE = "\033[1;34m"
+    CYAN = "\033[1;36m"
+    GREEN = "\033[0;32m"
+    RESET = "\033[0;0m"
+    BOLD = "\033[;1m"
+    REVERSE = "\033[;7m"
+    ENDC = "\033[0m"
+else:
+    RED = BLUE = CYAN = GREEN = RESET = BOLD = REVERSE = ENDC = ""
 
 
 def print_title(text: str):
@@ -16,7 +26,7 @@ def print_title(text: str):
     max_length = 73
     padded = f" {text} "
     n = int((max_length - len(padded)) / 2)
-    print(f"{character*n}{padded}{character*n}")
+    print(f"{character * n}{padded}{character * n}")
 
 
 def print_results(test_cases: list[Result]):
